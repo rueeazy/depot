8d56224603c65074232409311b5f12ac0f40be5d
Christian Ruiz <santacruz.crm@gmail.com>
2022-11-04 13:14:19 -0700

    add styling and update index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+["all", Rails.env].each do |seed|
+  seed_file = Rails.root.join("db", "seeds", "#{seed}.rb")
+
+  if File.exist?(seed_file)
+    require seed_file
+
+  end
+end
