856b572b47f37eac2517e0cee339d765c2260473
Christian Ruiz <santacruz.crm@gmail.com>
2022-11-10 14:01:24 -0800

    add Background job to send order confirmation email && add api for payment processing

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,9 @@
 require "application_system_test_case"
 
+
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+    
   setup do
     @order = orders(:one)
   end
@@ -18,7 +21,14 @@ class OrdersTest < ApplicationSystemTestCase
 
     assert_text "Order was successfully destroyed"
   end
+
   test "check routing number" do
+
+    LineItem.delete_all
+    Order.delete_all
+
+    visit store_index_url
+
     visit store_index_url
 
     click_on 'Add to Cart', match: :first
@@ -34,5 +44,29 @@ class OrdersTest < ApplicationSystemTestCase
     select 'Check', from: 'Pay type'
 
     assert_selector "#order_routing_number"
+
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas",      order.name
+    assert_equal "123 Main Street",  order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check",            order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"],                 mail.to
+    assert_equal 'Sam Ruby <depot@example.com>',       mail[:from].value
+    assert_equal "Book Store Order Confirmation", mail.subject
+
   end 
 end
